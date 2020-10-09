a1ab0b62ec50aca011598c2654bfcfc16f968949
Ewan Harris <ewanharris93@gmail.com>
2020-10-09 12:17:40 +0100

    test: replace options with parserOptions

--- test/no-unused-vars.js
+++ test/no-unused-vars.js
@@ -9,25 +9,25 @@ ruleTester.run('no-unused-vars', rule, {
 	valid: [
 		{
 			code: 'function foo() { }; var x; x=2; if(x) {  }',
-			options: [ { 'alloy/no-unused-vars': true } ],
+			parserOptions: { 'alloy/no-unused-vars': true },
 			filename: path.join(appFixtureDir, 'app', 'controllers', 'index.js')
 		},
 		{
 			code: 'function foo() { }; var x; x=2; if(x) {  }',
-			options: [ { 'alloy/no-unused-vars': true } ],
+			parserOptions: { 'alloy/no-unused-vars': true },
 			filename: path.join(appFixtureDir, 'app', 'controllers', 'foo.js')
 		}
 	],
 	invalid: [
 		{
 			code: 'function noFoo() { }',
-			options: [ { 'alloy/no-unused-vars': true } ],
+			parserOptions: { 'alloy/no-unused-vars': true },
 			filename: path.join(appFixtureDir, 'app', 'controllers', 'index.js'),
 			errors: [ { line: 1, column: 10 } ],
 		},
 		{
 			code: 'function foo() { }; var x; x=2; if(x) {  }',
-			options: [ { 'alloy/no-unused-vars': true } ],
+			parserOptions: { 'alloy/no-unused-vars': true },
 			filename: path.join(appFixtureDir, 'app', 'controllers', 'subdir', 'bar.js'),
 			errors: [ { line: 1, column: 10 } ],
 		}
